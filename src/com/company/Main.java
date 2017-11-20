3e16ffbd44c2075f2a874c7e0683d3ee05acb844
Pranav Patil <ompranav97@gmail.com>
2017-11-20 23:54:03 +0000

    Main.java edited online with Bitbucket The formatting works

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,4 +1,4 @@
-package com.company;
+//package com.company;
 import java.util.Random;
 
 /* Priority Scheduler without Pre-emption */
@@ -22,11 +22,11 @@ class process{
         priority = p.priority;
     }
     void print(){
-        System.out.println("Id: " + id);
-        System.out.println("\tBurst time: " + burst);
-        System.out.println("\tArrival Time: " + arrival);
+        System.out.print("Id: " + id);
+        System.out.print("\tBurst time: " + burst);
+        System.out.print("\tArrival Time: " + arrival);
         System.out.println("\tPriority level: " + priority);
-        System.out.println("\n");
+//        System.out.println("\n");
     }
 }
 
@@ -46,68 +46,97 @@ class scheduler{
         tail = number - 1;
         process[] processList;
         processList = new process[N];
+        float waiting = 0;
+        float turnaround = 0;
         //Instantiate all processes
-        Random rng = new Random(1);
-        for (int i = 0; i < N ; i++){
-            int id=i;
-            int b=rng.nextInt(101);
-            int a=rng.nextInt(1001);
-            int p=rng.nextInt(11);
+        for(int k = 0; k < 5; k++) {
+            Random rng = new Random(k);
+            for (int i = 0; i < N; i++) {
+                int id = i;
+                int b = rng.nextInt(101);
+                int a = rng.nextInt(1001);
+                int p = rng.nextInt(11);
 
-            processList[i] = new process(id,b,a,p);
-        }
-        //Sort by Arrival
-        for(int i = 0;i < 50;i++) {
-            for(int j = 0; j < 50; j++){
-                if(processList[i].arrival < processList[j].arrival){
-                    process temp = new process(processList[i]);
-                    processList[i] = processList[j];
-                    processList[j] = temp;
-                }
+                processList[i] = new process(id, b, a, p);
             }
-        }
-        //Initialize time to first arrived process time
-        time = processList[head].arrival;
-
-        /* Sorting Algorithm
-        * After sorting by arrival time, head and tail are used to
-        * indicate the "subarray" that the CPU is sorting by priority
-        * with respect to the time that has elapsed. */
-
-        while(head != 49) {
-            //Find tail
+            //Sort by Arrival
             for (int i = 0; i < 50; i++) {
-                if (processList[i].arrival <= time) {
-                    tail = i;
-                } else {
-                    break;
-                }
-            }
-            //Sort by Priority
-            for (int i = head; i <= tail; i++) {
-                for (int j = head; j <= tail; j++) {
-                    if (processList[i].priority < processList[j].priority) {
+                for (int j = 0; j < 50; j++) {
+                    if (processList[i].arrival < processList[j].arrival) {
                         process temp = new process(processList[i]);
                         processList[i] = processList[j];
                         processList[j] = temp;
                     }
                 }
             }
-            time = time + processList[head].burst; // Timer after burst
-            head = head + 1; //Increment head
-        }
+            //Initialize time to first arrived process time
+            time = processList[head].arrival;
+            /* Sorting Algorithm
+            * After sorting by arrival time, head and tail are used to
+            * indicate the "subarray" that the CPU is sorting by priority
+            * with respect to the time that has elapsed. */
 
-        //Print Sorted
-        // Todo: Format output as table
-        for (int i = 0; i < N ; i++){
-            processList[i].print();
+            while (head != 49) {
+                //Find tail
+                for (int i = 0; i < 50; i++) {
+                    if (processList[i].arrival <= time) {
+                        tail = i;
+                    } else {
+                        break;
+                    }
+                }
+                //Sort by Priority
+                for (int i = head; i <= tail; i++) {
+                    for (int j = head; j <= tail; j++) {
+                        if (processList[i].priority < processList[j].priority) {
+                            process temp = new process(processList[i]);
+                            processList[i] = processList[j];
+                            processList[j] = temp;
+                        }
+                    }
+                }
+                time = time + processList[head].burst; // Timer after burst
+                head = head + 1; //Increment head
+            }
+
+            time = processList[0].arrival;
+
+            //calculate wait time and turnaround time
+            for (int i = 0; i < N; i++)
+            {
+                waiting = waiting + time - processList[i].arrival;
+                time = time + processList[i].burst;
+                turnaround = turnaround + time - processList[i].arrival;
+            }
+
+            turnaround = turnaround / N;
+            waiting = waiting / N;
+
+            //Print Sorted
+            // Todo: Format output as table
+//            for (int i = 0; i < N; i++) {
+//                processList[i].print();
+//            }
+            //Table Output
+            System.out.print(N);
+            System.out.print("\t\t" + (k+1));
+            System.out.print("\t\t\t" + waiting + " ns");
+            System.out.println("\t\t" + turnaround + "ns");
+
         }
     }
 }
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        System.out.print("N");
+        System.out.print("\t\tRun Number\t\t");
+        System.out.print("Waiting Time");
+        System.out.println("\t\tTurnaround Time\t\t");
         scheduler test_scheduler = new scheduler(50);
+        scheduler test2 = new scheduler( 100);
+        scheduler test3 = new scheduler( 150);
     }
 }
